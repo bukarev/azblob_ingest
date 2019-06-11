13fb405675262cdbad997d6843e208cdd4862b8f
bukarev <37640806+bukarev@users.noreply.github.com>
2019-06-11 22:26:43 +1000

    Update nx_live.sh

--- nx_live.sh
+++ nx_live.sh
@@ -1,2 +1,2 @@
 export MAPR_TICKET_LOCATION=/tmp/maprticket_5000
-MASTER=yarn-client /opt/mapr/spark/spark-2.3.1/bin/spark-submit --executor-memory 12G --num-executors 40 --class nxLive /home/mapr/nx_live/target/scala-2.11/netxpress-live-data-process_2.11-0.0.1.jar $1 $2
+MASTER=yarn-client /opt/mapr/spark/spark-2.3.1/bin/spark-submit --executor-memory 12G --num-executors 40 --class logsLive /home/mapr/logs_process/target/scala-2.11/logs-live-data-process_2.11-0.0.1.jar $1 $2
